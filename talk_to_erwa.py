09c7b9dbed2d0e483880819f3ff48cd340a11600
mriehl <max@riehl.io>
2014-11-25 12:06:35 +0100

    change topic and add dummy full-update data

--- talk_to_erwa.py
+++ talk_to_erwa.py
@@ -7,6 +7,23 @@ import logging
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
+dummy_data = [[
+    {u'services':
+     [
+         {u'state': u'up', u'uri': u'service://machine/frontend', u'name': u'frontend'},
+         {u'state': u'down', u'uri': u'service://machine/iptables', u'name': u'iptables'},
+         {u'state': u'up', u'uri': u'service://machine/middleservice1', u'name': u'middleservice1'},
+         {u'state': u'up', u'uri': u'service://machine/middleservice2', u'name': u'middleservice2'},
+         {u'state': u'up', u'uri': u'service://machine/backservice', u'name': u'backservice'},
+         {u'state': u'up', u'uri': u'service://machine/docker', u'name': u'docker'}
+     ],
+     u'artefacts': [
+         {u'current': u'0.9.1-42.el9.x86_64', u'uri': u'artefact://machine/ConsoleKit-libs', u'name': u'ConsoleKit-libs'},
+         {u'current': u'1.3.10-33.el9.x86_64', u'uri': u'artefact://machine/zsh', u'name': u'zsh'}
+     ],
+        u'name': u'machine'}
+]]
+
 try:
     host = sys.argv[1]
 except IndexError:
@@ -17,12 +34,13 @@ sys.path.insert(0, "src/main/python")
 
 from yadtbroadcastclient import WampBroadcaster
 
-w = WampBroadcaster(host, "8080", "the_topic")
+w = WampBroadcaster(host, "8080", "foo")
 w.onEvent = print
+
 
 def spammy(counter, is_up):
     w.sendServiceChange({"fooservice": ("UP" if is_up else "DOWN")})
-    w.sendFullUpdate({"counter": counter})
+    w.sendFullUpdate(dummy_data)
     counter = counter + 1
     reactor.callLater(5, spammy, counter, not is_up)
 
