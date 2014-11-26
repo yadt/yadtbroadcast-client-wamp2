cdac97bf283230778c8297ef8be3ed64c4c06032
Arne Hilmann <arne@hilmann.de>
2014-11-26 16:26:04 +0100

    add second host to full_update

--- talk_to_erwa.py
+++ talk_to_erwa.py
@@ -21,7 +21,22 @@ dummy_data = [[
          {u'current': u'0.9.1-42.el9.x86_64', u'uri': u'artefact://machine/ConsoleKit-libs', u'name': u'ConsoleKit-libs'},
          {u'current': u'1.3.10-33.el9.x86_64', u'uri': u'artefact://machine/zsh', u'name': u'zsh'}
      ],
-        u'name': u'machine'}
+     u'name': u'machine'},
+    {u'services':
+     [
+         {u'state': u'up', u'uri': u'service://host2/frontend', u'name': u'frontend'},
+         {u'state': u'down', u'uri': u'service://host2/iptables', u'name': u'iptables'},
+         {u'state': u'up', u'uri': u'service://host2/middleservice1', u'name': u'middleservice1'},
+         {u'state': u'up', u'uri': u'service://host2/middleservice2', u'name': u'middleservice2'},
+         {u'state': u'up', u'uri': u'service://host2/backservice', u'name': u'backservice'},
+         {u'state': u'up', u'uri': u'service://host2/docker', u'name': u'docker'}
+     ],
+     u'artefacts': [
+         {u'current': u'0.9.1-42.el9.x86_64', u'uri': u'artefact://host2/ConsoleKit-libs', u'name': u'ConsoleKit-libs'},
+         {u'current': u'1.3.10-33.el9.x86_64', u'uri': u'artefact://host2/zsh', u'name': u'zsh'}
+     ],
+        u'name': u'host2'},
+
 ]]
 
 try:
